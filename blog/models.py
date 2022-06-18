ce7cdaa55186b3fcf07bc3b78a9ee38e86e9649e
Joseph Ibochi <josephibochi@gmail.com>
2022-06-18 22:12:59 +0100

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+# from cgitb import text
+# from turtle import title
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField('text')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField();
